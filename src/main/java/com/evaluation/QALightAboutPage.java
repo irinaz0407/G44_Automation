351a0c6e13c3edb238719722a0c822307c3db614
irinaz0407 <irushesta@gmail.com>
2020-08-09 18:19:16 +0200

    Add QALightAboutPage file

---
+++ src/main/java/com/evaluation/QALightAboutPage.java
@@ -0,0 +1,13 @@
+package com.evaluation;
+
+public class QALightAboutPage {
+    /*
+    * проверяем что есть элементы меню на странице
+     */
+    public final static String ADMINISTRATION = "//a[text()=\"Администрация\"]";
+    public final static String OUR_PARTNERS = "//a[text()=\"Наши тренеры\"]";
+    public final static String GALLERY = "//a[text()=\"Галерея\"]";
+    public final static String FEEDBACK = "//a[text()=\"Отзывы\"]";
+
+}
+
