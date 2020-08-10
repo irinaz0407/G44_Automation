a82140f2e4df5684f3dd16944166dc81149d7cc4
irinaz0407 <irushesta@gmail.com>
2020-08-10 17:52:36 +0200

    Changed XPath in the QALightAboutPage file and added comments

--- src/main/java/com/evaluation/QALightAboutPage.java
+++ src/main/java/com/evaluation/QALightAboutPage.java
@@ -2,12 +2,21 @@ package com.evaluation;
 
 public class QALightAboutPage {
     /*
-    * проверяем что есть элементы меню на странице
+    * проверка перехода на страницу Администратор
+    */
+    public final static String ADMINISTRATION = "//li[@id='menu-item-5456']/a[text()='Администрация']";
+    /*
+     * проверка перехода на страницу Наши тренеры
      */
-    public final static String ADMINISTRATION = "//a[text()=\"Администрация\"]";
-    public final static String OUR_PARTNERS = "//a[text()=\"Наши тренеры\"]";
-    public final static String GALLERY = "//a[text()=\"Галерея\"]";
-    public final static String FEEDBACK = "//a[text()=\"Отзывы\"]";
+    public final static String OUR_PARTNERS = "//li[@id='menu-item-416']/a[text()='Наши тренеры']";
+    /*
+     * проверка перехода на страницу Галерея
+     */
+    public final static String GALLERY = "//li[@id='menu-item-491']/a[text()='Галерея']";
+    /*
+     * проверка перехода на страницу Отзывы
+     */
+    public final static String FEEDBACK = "//li[@id='menu-item-575']/a[text()='Отзывы']";
 
 }
 
