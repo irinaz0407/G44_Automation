3a4feecc2870d78361b7cab88ca77a1a9124e6a6
irinaz0407 <irushesta@gmail.com>
2020-09-20 15:21:05 +0200

    Added remote driver support (property remote = yes/no) Added multibrowser support

--- src/test/java/tests/ui/BaseTest.java
+++ src/test/java/tests/ui/BaseTest.java
@@ -7,10 +7,14 @@ import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -21,31 +25,54 @@ public abstract class BaseTest {
     protected final Logger log = LogManager.getLogger("Test");
 
     protected WebDriver driver;
+    DesiredCapabilities capabilities = new DesiredCapabilities();
 
     @Before
     public void setUp() {
-
-        switch (System.getProperty("browser", "chrome")) {
-            case "chrome":
-                System.setProperty("webdriver.chrome.driver",
+        if (System.getProperty("remote", "no").equals("no")) {
+            switch (System.getProperty("browser", "chrome")) {
+                case "chrome":
+                    System.setProperty("webdriver.chrome.driver",
 //                System.getProperty("user.dir") +
-                        "D:\\autotest\\drivers\\chrome\\v85\\chromedriver.exe");
-                this.driver = new ChromeDriver();
-                break;
+                            "D:\\autotest\\drivers\\chrome\\v85\\chromedriver.exe");
+                    this.driver = new ChromeDriver();
+                    break;
 
-            case "firefox":
-                System.setProperty("webdriver.gecko.driver",
+                case "firefox":
+                    System.setProperty("webdriver.gecko.driver",
 //                System.getProperty("user.dir") +
-                        "D:\\autotest\\drivers\\firefox\\v0.27.0\\geckodriver.exe");
-                this.driver = new FirefoxDriver();
-                break;
+                            "D:\\autotest\\drivers\\firefox\\v0.27.0\\geckodriver.exe");
+                    this.driver = new FirefoxDriver();
+                    break;
 
             }
-            driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-            driver.get("https://github.com/login");
+        }
+        else {
+            switch (System.getProperty("browser", "chrome")) {
+                case "chrome":
+                    capabilities.setBrowserName("chrome");
+                    capabilities.setVersion("85.0");
+                case "firefox":
+                    capabilities.setBrowserName("firefox");
+                    capabilities.setVersion("80.0");
+            }
+            capabilities.setCapability("enableVNC", true);
+            capabilities.setCapability("enableVideo", false);
+
+            try {
+                this.driver  = new RemoteWebDriver(
+                        URI.create("http://localhost:4444/wd/hub").toURL(),
+                        capabilities);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
 
         }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get("https://github.com/login");
+
+    }
 
     @After
     public void ternDown(){
